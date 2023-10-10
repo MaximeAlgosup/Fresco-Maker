fee96076de7679964a236163d534b41beffed5a2
MaximeCARON <maxime.caron@algosup.com>
2023-10-10 15:10:07 +0200

    Add Gui error class

--- Gui/GuiRunner.py
+++ Gui/GuiRunner.py
@@ -10,18 +10,18 @@ class GuiRunner:
     def onClosing():
         sys.exit(0)
 
-    def check(self):
+    def set_data(self):
         print(self.picture_path.get())
         print(self.team_nb.get())
         print(self.is_doc_generate.get())
+        self.solver_win.destroy()
+
 
     def __init__(self):
         self.team_nb = 1
         self.generate_doc = True
         self.solver_win = Tk()
         self.solver_win.title("Rubik's solver 3000")
-
-        self.path_error_msg = StringVar()
 
         self.solver_win.geometry("500x300")
         self.solver_win.configure(bg="#ffb121")
@@ -46,6 +46,6 @@ class GuiRunner:
 
         # Add login button
         Button(self.solver_win, text="Valid", font=("Arial", 25), borderwidth=1, relief="solid",
-               command=self.check).pack(side=LEFT, pady=(5, 20), padx=(190, 20))
+               command=self.set_data).pack(side=LEFT, pady=(5, 20), padx=(190, 20))
 
         self.solver_win.mainloop()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from Rubiks3DViewer.Rubiks3DViewer import RubiksViewer as viewer
 from PictureConverter.PictureConverter import PictureConverter as pic_converter
 from Gui.GuiRunner import GuiRunner
+from Gui.GuiError import GuiError
 
 start_win = GuiRunner()
+print("test")
+test = GuiError("ERROR: bad path for picture")
