6d49067608afa623df188ad35db7b74f2dad9160
Mathias <mathias.gagnepain@algosup.com>
2023-10-10 20:54:15 +0200

    Poo + Class Mapping

--- main.py
+++ main.py
@@ -6,3 +6,26 @@ converted_pic.tile(3, "./split_out")
 converted_pic.get_formatted_matrix()
 
 
+## NEW Solver
+
+from RubiksSolver.RubiksSolver import RubiksSolver as RSolver
+
+pattern = [
+    'B', 'B', 'B',
+    'B', 'U', 'U',
+    'B', 'U', 'B'
+]
+
+if __name__ == '__main__':
+    
+    solver = RSolver(pattern)
+    cube_to_find = solver.getCenter() + "4"
+    
+
+
+    
+
+   
+    # print(solver)
+
+
