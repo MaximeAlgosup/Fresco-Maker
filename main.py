1579a0aeeebc7ab211f769d9c398dadaacc79353
MaximeCARON <maxime.caron@algosup.com>
2023-10-10 14:57:48 +0200

    add new branch for GUI

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from Rubiks3DViewer.Rubiks3DViewer import RubiksViewer as viewer
 from PictureConverter.PictureConverter import PictureConverter as pic_converter
+from Gui.GuiRunner import GuiRunner
 
-converted_pic = pic_converter("./test_pic.png")
-converted_pic.tile(3, "./split_out")
-converted_pic.get_formatted_matrix()
-
-
+start_win = GuiRunner()
