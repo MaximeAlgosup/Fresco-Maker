1ec85955ac7ab78a1ea0dd910a60fa243754db95
MaximeCARON <maxime.caron@algosup.com>
2023-10-12 16:47:04 +0200

    Start crown generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Rubiks3DViewer.Rubiks3DViewer import RubiksViewer as viewer
 from PictureConverter.PictureConverter import PictureConverter as pic_converter
+from CubeConverter.CubeConverter import Converter
 import os
 from Gui.GuiRunner import GuiRunner
 from Gui.GuiError import GuiError
@@ -17,40 +18,45 @@ def solve_cube(team, result_folder):
     pic_splitter = pic_converter(pic_path)
     pic_splitter.tile(3, tmp_dir_path)
     matrix_data = pic_splitter.get_matrix()
-    for matrix in matrix_data:
+
+    test = Converter()
+    print(matrix_data[0])
+    test.face_to_cube(matrix_data[0][1])
+    # for matrix in matrix_data:
         # format matrix to give it to the solver form [[a, b, c], [d, e, f], [g, h, i]] to [a, b, c, d, e, f, g, h, i]
-        flattened_matrix = [element for row in matrix[1] for element in row]
+        # test = Converter()
+        # test.face_to_cube(matrix[1])
         # give matrix to solver
 
         # get is answer
-        result_cubes = ["OOOOOOOOOBBBBBBBBBRRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG", "BBBBBBBBBOOOOOOOOORRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG"]
-        result_length = len(result_cubes)
-        for j in range(result_length):
-            cube = result_cubes[j]
-            r_viewer = viewer()
-            r_viewer.set_new_pic(cube)
-            r_viewer.save_pic(tmp_dir_path, str(str(matrix[0][0])+"_"+str(matrix[0][1])+"_part"+str(j)+".png"))
-            r_viewer.close_plt()
+        # result_cubes = ["OOOOOOOOOBBBBBBBBBRRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG", "BBBBBBBBBOOOOOOOOORRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG"]
+        # result_length = len(result_cubes)
+        # for j in range(result_length):
+        #     cube = result_cubes[j]
+        #     r_viewer = viewer()
+        #     r_viewer.set_new_pic(cube)
+        #     # r_viewer.save_pic(tmp_dir_path, str(str(matrix[0][0])+"_"+str(matrix[0][1])+"_part"+str(j)+".png"))
+        #     r_viewer.close_plt()
 
     # delete tmp folder
     # os.rmdir(tmp_dir_path)
 
 
-start_win = GuiRunner()
-start_win.run_widow()
-user_data = start_win.get_data()
+# start_win = GuiRunner()
+# start_win.run_widow()
+# user_data = start_win.get_data()
+#
+# picture_path = str(user_data[0])
+# team_nb = int(user_data[1])
+# is_create_doc = bool(user_data[2])
 
-picture_path = str(user_data[0])
-team_nb = int(user_data[1])
-is_create_doc = bool(user_data[2])
-
-# picture_path = "./test_pic.png"
-# team_nb = 8
-# is_create_doc = True
+picture_path = "./test_pic.png"
+team_nb = 8
+is_create_doc = True
 
 # Check the path
 if not os.path.exists(str(picture_path)):
-    GuiError("ERROR: file not found at: " + str(picture_path))
+    GuiError("ERROR: file not found at:\n " + str(picture_path))
 
 # Check file is picture file
 if not str(picture_path).lower().endswith(('.png', '.jpg', '.jpeg')):
