78ae4350ea20c1b7f54eed546577f0c24c8c5f21
Mathias <mathias.gagnepain@algosup.com>
2023-10-12 16:58:36 +0200

    Add PdfGenerator

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ is_create_doc = bool(user_data[2])
 
 # Check the path
 if not os.path.exists(str(picture_path)):
-    GuiError("ERROR: file not found at: " + str(picture_path))
+    GuiError("ERROR: file not found at:\n" + str(picture_path))
 
 # Check file is picture file
 if not str(picture_path).lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -117,7 +117,7 @@ if is_create_doc:
     #     thread_calcul = threading.Thread(target=solve_cube, args=((i+1), str(res_folder_name + str(res_folder_number))))
     #     thread_tab.append(thread_calcul)
     #     thread_calcul.start()
-    #
+    
     # for thread in thread_tab:
     #     thread.join()
 <<<<<<< HEAD
@@ -141,4 +141,16 @@ if __name__ == '__main__':
     else:
         print("cannot be solve")
         print(result)
+<<<<<<< HEAD
 >>>>>>> 26a66fe (Merge from main)
+=======
+        
+        
+####### PDF Generator #####
+        
+pdf_generator = PDFGenerator("rubiks_A_1.pdf")
+pdf_generator.add_text("Text about the rubiks")
+pdf_generator.add_comment("Comment about the rubiks cube (maybe can be modify ton give instructions).")
+pdf_generator.add_image(pdf_generator.selectIcons("U'"), 25, 37)
+pdf_generator.generate_pdf()
+>>>>>>> 4fb1cee (Add PdfGenerator)
