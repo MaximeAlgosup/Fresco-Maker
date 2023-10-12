fb9aafd307295ed92cd12d9cf1787aa0f9935997
MaximeCARON <maxime.caron@algosup.com>
2023-10-12 11:56:27 +0200

    various updates

--- main.py
+++ main.py
@@ -4,6 +4,29 @@ import os
 from Gui.GuiRunner import GuiRunner
 from Gui.GuiError import GuiError
 from Gui.GuiWarning import GuiWarning
+import threading
+
+
+def solve_cube(team, result_folder):
+    tmp_dir_path = "./" + str(result_folder) + "/team" + str(team) + "/tmp"
+    pic_path = "./" + str(result_folder) + "/team" + str(team) + "/team" + str(team) + ".png"
+    # create tmp folder
+    os.mkdir(tmp_dir_path)
+
+    # split pic in cubes
+    pic_splitter = pic_converter(pic_path)
+    matrix_data = pic_splitter.tile(3, tmp_dir_path)
+    print(matrix_data)
+
+    # for matrix in matrix_data:
+    #     # give matrix to solver
+    #     # get is answer
+    #     print(matrix)
+    #     # result_cube =
+
+    # delete tmp folder
+    # os.rmdir(tmp_dir_path)
+
 
 # start_win = GuiRunner()
 # start_win.run_widow()
@@ -14,12 +37,12 @@ from Gui.GuiWarning import GuiWarning
 # is_create_doc = bool(user_data[2])
 
 picture_path = "./test_pic.png"
-team_nb = int(8)
-is_create_doc = False
+team_nb = 8
+is_create_doc = True
 
 # Check the path
 if not os.path.exists(str(picture_path)):
-    GuiError("ERROR: file not found at: " + str(user_data[0]))
+    GuiError("ERROR: file not found at: " + str(picture_path))
 
 # Check file is picture file
 if not str(picture_path).lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -47,3 +70,13 @@ for i in range(team_nb):
     os.mkdir("./" + res_folder_name + str(res_folder_number) + "/team" + str(i + 1))
 
 split.split(team_nb, str("./" + res_folder_name + str(res_folder_number)))
+
+if is_create_doc:
+    thread_tab = []
+    for i in range(team_nb):
+        thread_calcul = threading.Thread(target=solve_cube, args=((i+1), str(res_folder_name + str(res_folder_number))))
+        thread_tab.append(thread_calcul)
+        thread_calcul.start()
+
+    for thread in thread_tab:
+        thread.join()
