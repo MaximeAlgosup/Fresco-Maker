47f1fdf236de29c1f37a8d24a2b28997b917d50d
Mathias <mathias.gagnepain@algosup.com>
2023-10-16 14:55:10 +0200

    test

--- PictureConverter/PictureConverter.py
+++ PictureConverter/PictureConverter.py
@@ -5,6 +5,7 @@ from itertools import product
 
 
 class PictureConverter:
+    # Define color constants
     red = (213, 37, 37)
     green = (106, 190, 48)
     blue = (91, 110, 225)
@@ -14,6 +15,7 @@ class PictureConverter:
     black = (0, 0, 0)
 
     def __init__(self, picture_path):
+        # Initialize the PictureConverter with a picture path
         if os.path.exists(picture_path):
             self.picture_path = picture_path
             self.picture = Image.open(self.picture_path)
@@ -23,7 +25,70 @@ class PictureConverter:
             logging.error("Can't find the picture at the specified path")
             exit(1)
 
+    def split(self, team_nb, output_dir):
+        # Split the picture into sub-images for multiple teams
+        if team_nb == 1:
+            # Handle the case when there is only one team
+            self.picture.save(os.path.join(output_dir, "team1/team1.png"))
+            return
+        # Determine the number of rows and columns for the sub-images
+        if team_nb == 2:
+            rows, cols = 1, 2
+        else:
+            # Calculate rows and columns based on team number
+            if team_nb % 2 == 0:
+                rows = 2
+                cols = team_nb // 2
+            else:
+                rows = 1
+                cols = team_nb
+        # Define column and row sizes
+        # Define cols size
+        # set width in rubik's unit
+        r_width = self.width // 3
+        cols_size = r_width // cols
+        rest_cols = r_width % cols
+
+        cols_size_val = []
+        for i in range(int(cols)):
+            cols_size_val.append(cols_size * 3)
+        cols_size_val[-1] = int(cols_size_val[-1]) + int(rest_cols) * 3
+
+        # Define height size
+        # set height in rubik's unit
+        r_height = self.height // 3
+        rows_size = r_height // rows
+        rest_rows = r_height % rows
+
+        rows_size_val = []
+        for i in range(int(rows)):
+            rows_size_val.append(rows_size * 3)
+
+        rows_size_val[-1] = int(rows_size_val[-1]) + int(rest_rows) * 3
+
+        coords = []
+
+        start_row = 0
+        for row in rows_size_val:
+            start_col = 0
+            end_row = start_row + row
+            for col in cols_size_val:
+                end_col = start_col + col
+                coords.append([start_col, start_row, end_col, end_row])
+                start_col = end_col
+            start_row = end_row
+        # Initialize a counter for team numbering
+        nb = 1
+        for cood in coords:
+            # Crop the sub-image
+            sub_image = self.picture.crop((cood[0], cood[1], cood[2], cood[3]))
+            # Save the sub-image to the appropriate team directory
+            sub_image.save(os.path.join(output_dir, f"team{nb}/team{nb}.png"))
+            # Increment the team number counter
+            nb += 1
+
     def tile(self, block_size, out_folder, need_matrix_conversion=True):
+        # Tile the picture into smaller blocks
         if not os.path.exists(out_folder):
             logging.error("Can't find the out folder")
             exit(1)
@@ -31,19 +96,30 @@ class PictureConverter:
 
         grid = product(range(0, self.height - self.height % block_size, block_size),
                        range(0, self.width - self.width % block_size, block_size))
+        # Get coordinates of smaller blocks
         for i, j in grid:
             box = (j, i, j + block_size, i + block_size)
             crop_pic = self.picture.crop(box)
             if need_matrix_conversion:
-                self.color_matrix.append(self.__to_color_matrix(i, j, crop_pic, block_size))
+                # Append color matrix information
+                self.color_matrix.append([[i, j], self.__to_color_matrix(crop_pic, block_size)])
             else:
+                # Save the cropped picture
                 out = os.path.join(out_folder, f'{i}_{j}{ext}')
                 crop_pic.save(out)
 
+    def test_rubiks_resolution(self):
+        # Check if the picture resolution is compatible with a Rubik's Cube
+        if self.width % 3 > 0 or self.height % 3 > 0:
+            return False
+        return True
+
     def get_matrix(self):
+        # Get the color matrix
         return self.color_matrix
 
     def get_formatted_matrix(self):
+        # Get the color matrix in a formatted representation
         res = []
         for colors in self.color_matrix:
             face = ""
@@ -53,7 +129,8 @@ class PictureConverter:
             res.append(face)
         return res
 
-    def __to_color_matrix(self, x, y, crop_pic, block_size):
+    def __to_color_matrix(self, crop_pic, block_size):
+        # Convert a cropped picture into a color matrix
         matrix = []
         for i in range(0, block_size):
             line = []
@@ -63,14 +140,17 @@ class PictureConverter:
         return matrix
 
     def __get_pixel_color_char(self, pic, x, y):
+        # Get the color character for a specific pixel
         color_matrix = self.__get_pixel_color(pic, x, y)
         return self.__color_to_char(color_matrix)
 
     @staticmethod
     def __get_pixel_color(pic, x, y):
+        # Get the color of a specific pixel in the picture
         return pic.getpixel((x, y))
 
     def __color_to_char(self, color_matrix):
+        # Convert a color to a character representation
         match color_matrix:
             case self.red:
                 return 'R'
--- main.py
+++ main.py
@@ -3,6 +3,38 @@ from PictureConverter.PictureConverter import PictureConverter as pic_converter
 import os
 from Gui.GuiRunner import GuiRunner
 from Gui.GuiError import GuiError
+from Gui.GuiWarning import GuiWarning
+import threading
+
+
+def solve_cube(team, result_folder):
+    tmp_dir_path = "./" + str(result_folder) + "/team" + str(team) + "/tmp"
+    pic_path = "./" + str(result_folder) + "/team" + str(team) + "/team" + str(team) + ".png"
+    # create tmp folder
+    os.mkdir(tmp_dir_path)
+
+    # split pic in cubes
+    pic_splitter = pic_converter(pic_path)
+    pic_splitter.tile(3, tmp_dir_path)
+    matrix_data = pic_splitter.get_matrix()
+    for matrix in matrix_data:
+        # format matrix to give it to the solver form [[a, b, c], [d, e, f], [g, h, i]] to [a, b, c, d, e, f, g, h, i]
+        flattened_matrix = [element for row in matrix[1] for element in row]
+        # give matrix to solver
+
+        # get is answer
+        result_cubes = ["OOOOOOOOOBBBBBBBBBRRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG", "BBBBBBBBBOOOOOOOOORRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG"]
+        result_length = len(result_cubes)
+        for j in range(result_length):
+            cube = result_cubes[j]
+            r_viewer = viewer()
+            r_viewer.set_new_pic(cube)
+            r_viewer.save_pic(tmp_dir_path, str(str(matrix[0][0])+"_"+str(matrix[0][1])+"_part"+str(j)+".png"))
+            r_viewer.close_plt()
+
+    # delete tmp folder
+    # os.rmdir(tmp_dir_path)
+
 
 <<<<<<< HEAD
 start_win = GuiRunner()
@@ -14,17 +46,26 @@ user_data = start_win.get_data()
 # converted_pic.get_formatted_matrix()
 >>>>>>> 51d308a (Relation Mappin-Solver)
 
+picture_path = str(user_data[0])
+team_nb = int(user_data[1])
+is_create_doc = bool(user_data[2])
+
+# picture_path = "./test_pic.png"
+# team_nb = 8
+# is_create_doc = True
+
 # Check the path
-if not os.path.exists(str(user_data[0])):
-    GuiError("ERROR: file not found at: "+str(user_data[0]))
+if not os.path.exists(str(picture_path)):
+    GuiError("ERROR: file not found at: " + str(picture_path))
 
 # Check file is picture file
-if not str(user_data[0]).lower().endswith(('.png', '.jpg', '.jpeg')):
+if not str(picture_path).lower().endswith(('.png', '.jpg', '.jpeg')):
     GuiError("ERROR: file must be a picture")
 
 # Check team member nb
-if int(user_data[1]) < 1:
+if team_nb < 1:
     GuiError("ERROR: team number must be \n equal or upper than 1")
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 =======
 =======
@@ -50,3 +91,38 @@ if __name__ == '__main__':
 
 >>>>>>> 6d49067 (Poo + Class Mapping)
 >>>>>>> Stashed changes
+=======
+
+# Check if "result" folder already exist if it exists add 0, 1, 2... to the name
+res_folder_name = "result"
+res_folder_number = 0
+while os.path.exists("./" + res_folder_name + str(res_folder_number)):
+    res_folder_number += 1
+
+os.mkdir("./" + res_folder_name + str(res_folder_number))
+
+# Start to launch the splitter and check the picture size
+split = pic_converter(picture_path)
+if not split.test_rubiks_resolution():
+    GuiError("ERROR: the resolution of the selected\nimage is not achievable in rubik's cube")
+
+GuiWarning("WARNING: if the colors do not\ncorrespond to the colors of the\nRubik's cube they can be modified")
+for i in range(team_nb):
+    os.mkdir("./" + res_folder_name + str(res_folder_number) + "/team" + str(i + 1))
+
+split.split(team_nb, str("./" + res_folder_name + str(res_folder_number)))
+
+if is_create_doc:
+
+    for i in range(team_nb):
+        solve_cube((i + 1), str(res_folder_name + str(res_folder_number)))
+
+    # thread_tab = []
+    # for i in range(team_nb):
+    #     thread_calcul = threading.Thread(target=solve_cube, args=((i+1), str(res_folder_name + str(res_folder_number))))
+    #     thread_tab.append(thread_calcul)
+    #     thread_calcul.start()
+    #
+    # for thread in thread_tab:
+    #     thread.join()
+>>>>>>> b4472d839cff802c045293b0bb16474e1adabba6
