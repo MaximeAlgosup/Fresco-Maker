756a22cf1305683dcf91f63e2e87477a569bcc3d
MaximeCARON <maxime.caron@algosup.com>
2023-10-16 15:57:15 +0200

    Add commentary

--- Gui/GuiError.py
+++ Gui/GuiError.py
@@ -7,20 +7,27 @@ class GuiError:
     # Add close all app on closing window
     @staticmethod
     def onClosing():
+        # Define a function to exit the application when the window is closed
         sys.exit(0)
 
     def __init__(self, error_message):
+        # Initialize the error message window
         self.errors_win = Tk()
-        self.errors_win.title("Rubik's solver 3000: ERROR")
+        self.errors_win.title("Rubik's asolver 3000: ERROR")
 
+        # Set the window dimensions and background color
         self.errors_win.geometry("400x75")
         self.errors_win.configure(bg="#ffb121")
+
+        # Disable window resizing and set the window icon
         self.errors_win.resizable(False, False)
         self.errors_win.iconbitmap('./logo/rubiks.ico')
 
         # Add action on window close event
         self.errors_win.protocol("WM_DELETE_WINDOW", self.onClosing)
 
+        # Display the error message as a label with specified font and background color
         Label(self.errors_win, text=error_message, font=("Arial", 15, "bold"), bg="#ffb121").pack()
 
+        # Start the error window's main event loop
         self.errors_win.mainloop()
--- Gui/GuiRunner.py
+++ Gui/GuiRunner.py
@@ -7,26 +7,32 @@ class GuiRunner:
     # Add close all app on closing window
     @staticmethod
     def onClosing():
+        # Define a function to exit the application when the window is closed
         sys.exit(0)
 
     def get_data(self):
+        # Get user input data
         return [self.picture_path, self.team_nb, self.is_doc_generate]
 
     def set_data(self):
+        # Set data based on user input
         self.picture_path = self.picture_path.get()
         self.team_nb = self.team_nb.get()
         self.is_doc_generate = self.is_doc_generate.get()
         self.solver_win.destroy()
 
     def run_widow(self):
+        # Start the main event loop for the GUI window
         self.solver_win.mainloop()
 
     def __init__(self):
+        # Initialize default values and the solver window
         self.team_nb = 1
         self.generate_doc = True
         self.solver_win = Tk()
         self.solver_win.title("Rubik's solver 3000")
 
+        # Configure window dimensions and appearance
         self.solver_win.geometry("500x300")
         self.solver_win.configure(bg="#ffb121")
         self.solver_win.resizable(False, False)
@@ -35,6 +41,7 @@ class GuiRunner:
         # Add action on window close event
         self.solver_win.protocol("WM_DELETE_WINDOW", self.onClosing)
 
+        # Create and configure GUI elements: labels, input fields, checkboxes, and buttons
         Label(self.solver_win, text="Picture path:", font=("Arial", 15, "bold"), bg="#ffb121").pack(padx=(55, 300))
         self.picture_path = Entry(self.solver_win, font=("Arial", 25), borderwidth=3, relief="solid")
         self.picture_path.pack(pady=(0, 15))
@@ -51,6 +58,6 @@ class GuiRunner:
                                           bg="#ffb121")
         self.doc_generation.pack()
 
-        # Add login button
+        # Add validation button
         Button(self.solver_win, text="Valid", font=("Arial", 25), borderwidth=1, relief="solid",
                command=self.set_data).pack(side=LEFT, pady=(5, 20), padx=(190, 20))
