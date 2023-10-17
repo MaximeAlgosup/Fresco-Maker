23b20af0ab8fd08e9b38735b86d5c58d1036127c
MaximeCARON <maxime.caron@algosup.com>
2023-10-17 16:07:03 +0200

    Add u, L, and D movement to the cube

---
+++ CubeConverter/Cube.py
@@ -0,0 +1,303 @@
+class Cube:
+    def __init__(self):
+        self.UP = 0
+        self.LEFT = 1
+        self.FACE = 2
+        self.RIGHT = 3
+        self.BACK = 4
+        self.DOWN = 5
+        self.cube = [
+            ['W', 'W', 'W',
+             'W', 'W', 'W',
+             'W', 'W', 'W'],
+            ['B', 'B', 'B',
+             'B', 'B', 'B',
+             'B', 'B', 'B'],
+            ['O', 'O', 'O',
+             'O', 'O', 'O',
+             'O', 'O', 'O'],
+            ['G', 'G', 'G',
+             'G', 'G', 'G',
+             'G', 'G', 'G'],
+            ['R', 'R', 'R',
+             'R', 'R', 'R',
+             'R', 'R', 'R'],
+            ['Y', 'B', 'Y',
+             'Y', 'Y', 'Y',
+             'Y', 'R', 'Y'],
+        ]
+
+        self.moves_history = []
+
+        # if not self.__base_check():
+        #     exit(1)
+
+    # moves
+
+    # Move U x times
+    def move_u(self, x=1):
+        for _ in range(x):
+            new_up_face = self.__rotate_matrix(self.UP)
+            # Define the faces to update in a loop
+            face_order = [self.LEFT, self.FACE, self.RIGHT, self.BACK]
+            new_faces = []
+
+            for face_nb in face_order:
+                new_face = [
+                    self.cube[face_order[(face_order.index(face_nb) + 1) % 4]][0],
+                    self.cube[face_order[(face_order.index(face_nb) + 1) % 4]][1],
+                    self.cube[face_order[(face_order.index(face_nb) + 1) % 4]][2],
+                    self.cube[face_nb][3],
+                    self.cube[face_nb][4],
+                    self.cube[face_nb][5],
+                    self.cube[face_nb][6],
+                    self.cube[face_nb][7],
+                    self.cube[face_nb][8]
+                ]
+                new_faces.append(new_face)
+
+            # Update the faces in the cube
+            self.cube[self.UP] = new_up_face
+            for i in range(4):
+                self.cube[face_order[i]] = new_faces[i]
+
+            # Add the move to history
+            self.moves_history.append("U")
+
+    # Move D x times
+    def move_d(self, x=1):
+        for _ in range(x):
+            new_down_face = self.__rotate_matrix(self.DOWN)
+            # Define the faces to update in a loop
+            face_order = [self.LEFT, self.FACE, self.RIGHT, self.BACK]
+            new_faces = []
+
+            for face_nb in face_order:
+                new_face = [
+                    self.cube[face_nb][0],
+                    self.cube[face_nb][1],
+                    self.cube[face_nb][2],
+                    self.cube[face_nb][3],
+                    self.cube[face_nb][4],
+                    self.cube[face_nb][5],
+                    self.cube[face_order[(face_order.index(face_nb) - 1) % 4]][6],
+                    self.cube[face_order[(face_order.index(face_nb) - 1) % 4]][7],
+                    self.cube[face_order[(face_order.index(face_nb) - 1) % 4]][8]
+                ]
+                new_faces.append(new_face)
+
+            # Update the faces in the cube
+            self.cube[self.DOWN] = new_down_face
+            for i in range(4):
+                self.cube[face_order[i]] = new_faces[i]
+
+            # Add the move to history
+            self.moves_history.append("D")
+
+    # Move U' x times
+    def move_u_p(self, x=1):
+        for _ in range(x):
+            new_up_face = self.__rotate_matrix_reverse(self.UP)
+            # Define the faces to update in a loop
+            face_order = [self.LEFT, self.FACE, self.RIGHT, self.BACK]
+            new_faces = []
+
+            for face_nb in face_order:
+                new_face = [
+                    self.cube[face_order[(face_order.index(face_nb) - 1) % 4]][0],
+                    self.cube[face_order[(face_order.index(face_nb) - 1) % 4]][1],
+                    self.cube[face_order[(face_order.index(face_nb) - 1) % 4]][2],
+                    self.cube[face_nb][3],
+                    self.cube[face_nb][4],
+                    self.cube[face_nb][5],
+                    self.cube[face_nb][6],
+                    self.cube[face_nb][7],
+                    self.cube[face_nb][8]
+                ]
+                new_faces.append(new_face)
+
+            # Update the faces in the cube
+            self.cube[self.UP] = new_up_face
+            for i in range(4):
+                self.cube[face_order[i]] = new_faces[i]
+
+            # Add the move to history
+            self.moves_history.append("u'")
+
+    # Move D' x times
+    def move_d_p(self, x=1):
+        for _ in range(x):
+            new_down_face = self.__rotate_matrix_reverse(self.DOWN)
+            # Define the faces to update in a loop
+            face_order = [self.LEFT, self.FACE, self.RIGHT, self.BACK]
+            new_faces = []
+            for face_nb in face_order:
+                new_face = [
+                    self.cube[face_nb][0],
+                    self.cube[face_nb][1],
+                    self.cube[face_nb][2],
+                    self.cube[face_nb][3],
+                    self.cube[face_nb][4],
+                    self.cube[face_nb][5],
+                    self.cube[face_order[(face_order.index(face_nb) + 1) % 4]][6],
+                    self.cube[face_order[(face_order.index(face_nb) + 1) % 4]][7],
+                    self.cube[face_order[(face_order.index(face_nb) + 1) % 4]][8]
+                ]
+                new_faces.append(new_face)
+                # Update the faces in the cube
+            self.cube[self.DOWN] = new_down_face
+            for i in range(4):
+                self.cube[face_order[i]] = new_faces[i]
+                # Add the move to history
+            self.moves_history.append("D'")
+
+    # Move L x times
+    def move_l(self, x=1):
+        for i in range(x):
+            # move left face
+            new_left_face = self.__rotate_matrix(self.LEFT)
+            # move up face
+            face_nb = self.UP
+            new_up_face = [self.cube[self.BACK][2], self.cube[face_nb][1], self.cube[face_nb][2],
+                           self.cube[self.BACK][5], self.cube[face_nb][4], self.cube[face_nb][5],
+                           self.cube[self.BACK][8], self.cube[face_nb][7], self.cube[face_nb][8]]
+            # move main face
+            face_nb = self.FACE
+            new_main_face = [self.cube[self.UP][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                             self.cube[self.UP][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                             self.cube[self.UP][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+            # move back face
+            face_nb = self.BACK
+            new_back_face = [self.cube[face_nb][0], self.cube[face_nb][1], self.cube[self.DOWN][0],
+                             self.cube[face_nb][3], self.cube[face_nb][4], self.cube[self.DOWN][3],
+                             self.cube[face_nb][6], self.cube[face_nb][7], self.cube[self.DOWN][6]]
+            # move down face
+            face_nb = self.DOWN
+            new_down_face = [self.cube[self.FACE][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                             self.cube[self.FACE][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                             self.cube[self.FACE][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+
+            self.cube[self.UP] = new_up_face
+            self.cube[self.LEFT] = new_left_face
+            self.cube[self.FACE] = new_main_face
+            self.cube[self.BACK] = new_back_face
+            self.cube[self.DOWN] = new_down_face
+
+            # Add the move to history
+            self.moves_history.append("L")
+
+    # Move L' x times
+    def move_l_p(self, x=1):
+        for i in range(x):
+            # move left face
+            new_left_face = self.__rotate_matrix_reverse(self.LEFT)
+            # move up face
+            face_nb = self.UP
+            new_up_face = [self.cube[self.FACE][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                           self.cube[self.FACE][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                           self.cube[self.FACE][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+            # move main face
+            face_nb = self.FACE
+            new_main_face = [self.cube[self.DOWN][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                             self.cube[self.DOWN][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                             self.cube[self.DOWN][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+            # move back face
+            face_nb = self.BACK
+            new_back_face = [self.cube[face_nb][0], self.cube[face_nb][1], self.cube[self.UP][2],
+                             self.cube[face_nb][3], self.cube[face_nb][4], self.cube[self.UP][5],
+                             self.cube[face_nb][6], self.cube[face_nb][7], self.cube[self.UP][8]]
+            # move down face
+            face_nb = self.DOWN
+            new_down_face = [self.cube[self.BACK][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                             self.cube[self.BACK][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                             self.cube[self.BACK][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+
+            self.cube[self.UP] = new_up_face
+            self.cube[self.LEFT] = new_left_face
+            self.cube[self.FACE] = new_main_face
+            self.cube[self.BACK] = new_back_face
+            self.cube[self.DOWN] = new_down_face
+
+            # Add the move to history
+            self.moves_history.append("L'")
+
+    def move_r(self, x=1):
+        for i in range(x):
+            # move right face
+            new_left_face = self.__rotate_matrix_reverse(self.RIGHT)
+            # move up face
+            face_nb = self.UP
+            new_up_face = [self.cube[self.FACE][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                           self.cube[self.FACE][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                           self.cube[self.FACE][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+            # move main face
+            face_nb = self.FACE
+            new_main_face = [self.cube[self.DOWN][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                             self.cube[self.DOWN][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                             self.cube[self.DOWN][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+            # move back face
+            face_nb = self.BACK
+            new_back_face = [self.cube[face_nb][0], self.cube[face_nb][1], self.cube[self.UP][2],
+                             self.cube[face_nb][3], self.cube[face_nb][4], self.cube[self.UP][5],
+                             self.cube[face_nb][6], self.cube[face_nb][7], self.cube[self.UP][8]]
+            # move down face
+            face_nb = self.DOWN
+            new_down_face = [self.cube[self.BACK][0], self.cube[face_nb][1], self.cube[face_nb][2],
+                             self.cube[self.BACK][3], self.cube[face_nb][4], self.cube[face_nb][5],
+                             self.cube[self.BACK][6], self.cube[face_nb][7], self.cube[face_nb][8]]
+
+            self.cube[self.UP] = new_up_face
+            self.cube[self.LEFT] = new_left_face
+            self.cube[self.FACE] = new_main_face
+            self.cube[self.BACK] = new_back_face
+            self.cube[self.DOWN] = new_down_face
+
+            # Add the move to history
+            self.moves_history.append("L'")
+
+    # getters
+    def get_cube(self):
+        string_res = ""
+        for big_face in self.cube:
+            for face in big_face:
+                string_res += face
+        return string_res
+
+    def get_moves(self):
+        return self.moves_history
+
+    # private
+    def __rotate_matrix(self, face_nb):
+        return [self.cube[face_nb][6], self.cube[face_nb][3], self.cube[face_nb][0],
+                self.cube[face_nb][7], self.cube[face_nb][4], self.cube[face_nb][1],
+                self.cube[face_nb][8], self.cube[face_nb][5], self.cube[face_nb][2]]
+
+    def __rotate_matrix_reverse(self, face_nb):
+        return [self.cube[face_nb][2], self.cube[face_nb][5], self.cube[face_nb][8],
+                self.cube[face_nb][1], self.cube[face_nb][4], self.cube[face_nb][7],
+                self.cube[face_nb][0], self.cube[face_nb][3], self.cube[face_nb][6]]
+
+    def __base_check(self):
+        red = 0
+        green = 0
+        white = 0
+        blue = 0
+        orange = 0
+        yellow = 0
+        for big_face in self.cube:
+            for face in big_face:
+                match face:
+                    case 'R':
+                        red += 1
+                    case 'G':
+                        green += 1
+                    case 'W':
+                        white += 1
+                    case 'B':
+                        blue += 1
+                    case 'O':
+                        orange += 1
+                    case 'Y':
+                        yellow += 1
+
+        return red == 9 and green == 9 and white == 9 and blue == 9 and orange == 9 and yellow == 9
--- main.py
+++ main.py
@@ -1,52 +1,58 @@
 from Rubiks3DViewer.Rubiks3DViewer import RubiksViewer as viewer
 from PictureConverter.PictureConverter import PictureConverter as pic_converter
 import os
+from CubeConverter.Cube import Cube
 from Gui.GuiRunner import GuiRunner
 from Gui.GuiError import GuiError
 from Gui.GuiWarning import GuiWarning
-import threading
 
 
 def solve_cube(team, result_folder):
     tmp_dir_path = "./" + str(result_folder) + "/team" + str(team) + "/tmp"
     pic_path = "./" + str(result_folder) + "/team" + str(team) + "/team" + str(team) + ".png"
+    r_viewer = viewer()
+    cube = Cube()
+    cube.move_d_p()
+    r_viewer.set_new_pic(cube.get_cube())
+    r_viewer.show_pic()
+    r_viewer.close_plt()
     # create tmp folder
-    os.mkdir(tmp_dir_path)
-
-    # split pic in cubes
-    pic_splitter = pic_converter(pic_path)
-    pic_splitter.tile(3, tmp_dir_path)
-    matrix_data = pic_splitter.get_matrix()
-    for matrix in matrix_data:
-        # format matrix to give it to the solver form [[a, b, c], [d, e, f], [g, h, i]] to [a, b, c, d, e, f, g, h, i]
-        flattened_matrix = [element for row in matrix[1] for element in row]
-        # give matrix to solver
-
-        # get is answer
-        result_cubes = ["OOOOOOOOOBBBBBBBBBRRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG", "BBBBBBBBBOOOOOOOOORRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG"]
-        result_length = len(result_cubes)
-        for j in range(result_length):
-            cube = result_cubes[j]
-            r_viewer = viewer()
-            r_viewer.set_new_pic(cube)
-            r_viewer.save_pic(tmp_dir_path, str(str(matrix[0][0])+"_"+str(matrix[0][1])+"_part"+str(j)+".png"))
-            r_viewer.close_plt()
+    # os.mkdir(tmp_dir_path)
+    #
+    # # split pic in cubes
+    # pic_splitter = pic_converter(pic_path)
+    # pic_splitter.tile(3, tmp_dir_path)
+    # matrix_data = pic_splitter.get_matrix()
+    # for matrix in matrix_data:
+    #     # format matrix to give it to the solver form [[a, b, c], [d, e, f], [g, h, i]] to [a, b, c, d, e, f, g, h, i]
+    #     flattened_matrix = [element for row in matrix[1] for element in row]
+    #     # give matrix to solver
+    #
+    #     # get is answer
+    #     result_cubes = ["OOOOOOOOOBBBBBBBBBRRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG", "BBBBBBBBBOOOOOOOOORRRRRRRRRWWWWWWWWWYYYYYYYYYGGGGGGGGG"]
+    #     result_length = len(result_cubes)
+    #     for j in range(result_length):
+    #         cube = result_cubes[j]
+    #         r_viewer = viewer()
+    #         r_viewer.set_new_pic(cube)
+    #         r_viewer.save_pic(tmp_dir_path, str(str(matrix[0][0])+"_"+str(matrix[0][1])+"_part"+str(j)+".png"))
+    #         r_viewer.close_plt()
 
     # delete tmp folder
     # os.rmdir(tmp_dir_path)
 
 
-start_win = GuiRunner()
-start_win.run_widow()
-user_data = start_win.get_data()
+# start_win = GuiRunner()
+# start_win.run_widow()
+# user_data = start_win.get_data()
+#
+# picture_path = str(user_data[0])
+# team_nb = int(user_data[1])
+# is_create_doc = bool(user_data[2])
 
-picture_path = str(user_data[0])
-team_nb = int(user_data[1])
-is_create_doc = bool(user_data[2])
-
-# picture_path = "./test_pic.png"
-# team_nb = 8
-# is_create_doc = True
+picture_path = "./test_pic.png"
+team_nb = 8
+is_create_doc = True
 
 # Check the path
 if not os.path.exists(str(picture_path)):
